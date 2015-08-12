5cad4cc8d3f6c3a995786eba7a58be5307e53e0b
pablo31 <psfutn@gmail.com>
2015-08-11 23:05:24 -0300

    tn fetcher done

--- app/workers/tn_fetcher.rb
+++ app/workers/tn_fetcher.rb
@@ -17,44 +17,54 @@ class TNFetcher < SourceFetcher
     end
   end
 
-  def fetch_noticias_urls from, to
+  def fetch_sitemaps_urls from, to
     puts 'Fetching tn main sitemap ...'
     main_sitemap_url = 'http://www.tn.com.ar/sitemap.xml'
     main_sitemap = Nokogiri::HTML open main_sitemap_url
-    sitemaps_urls = main_sitemap.css('sitemapindex sitemap').select do |tr|
+    main_sitemap.css('sitemapindex sitemap').select do |tr|
       updated_time = Time.parse tr.css('lastmod').text
       updated_time >= from
     end.map{ |tr| tr.css('loc').text }
+  end
+
+  def fetch_noticias_urls_from_sitemap sitemap_url, from, to
+    puts "Fetching tn sitemap #{sitemap_url} ..."
+    sitemap = Nokogiri::HTML open sitemap_url
+    rows = sitemap.css 'urlset url'
+    urls = rows.select do |row|
+      updated_time = row.css('lastmod').text
+      updated_time = updated_time && !updated_time.empty? && Time.parse(updated_time)
+      updated_time && updated_time >= from && updated_time <= to
+    end.map do |row|
+      row.css('loc').text
+    end.select do |url|
+      invalid_sections = %w( programas tnylagente videos node )
+      invalid_sections.all?{ |section| !url.downcase.include? "/#{section}/" }
+    end
+    urls.each{ |url| puts "Noticia found: #{url}"}
+    urls
+  end
+
+  def fetch_noticias_urls from, to
+    sitemaps_urls = fetch_sitemaps_urls from, to
     noticias_urls = Array.new
     sitemaps_urls.reverse.each do |sitemap_url|
-      puts "Fetching tn sitemap #{sitemap_url} ..."
-      sitemap = Nokogiri::HTML open sitemap_url
-      rows = sitemap.css 'urlset url'
-      urls = rows.select do |row|
-        updated_time = row.css('lastmod').text
-        updated_time = updated_time && !updated_time.empty? && Time.parse(updated_time)
-        updated_time && updated_time >= from && updated_time <= to
-      end.map do |row|
-        row.css('loc').text
-      end.select do |url|
-        !url.include? 'tnylagente'
-      end
-      urls.each{ |url| puts "Noticia found: #{url}"}
-      noticias_urls << urls
+      urls = fetch_noticias_urls_from_sitemap sitemap_url, from, to
+      noticias_urls.push *urls
     end
-    noticias_urls.flatten
+    noticias_urls
   end
 
   def fetch_notice url
     puts "Fetching notice in #{url} ..."
-    # html = Nokogiri::HTML open url
-    # title = html.css('.entry-title').first.text
-    # body = format_body html.css('.entry-content .cuerposmart p')
-    # image = html.css('.hmedia img').first
-    # image = image && image.attr('src')
-    # media_items = Array image
-    # # Noticia.new title: title, body: body, source: :infobae, source_url: url, media_items: media_items
-    # { title: title, body: body, source: :infobae, source_url: url, media_items: media_items }
+    html = Nokogiri::HTML open url
+    title = html.css('.main-content .heading .entry-title').first.text
+    body = format_body html.css('.main-content .entry-content > p')
+    image = html.css('.main-content .hmedia img').first
+    image = image && image.attr('src')
+    media_items = Array image
+    # Noticia.new title: title, body: body, source: :infobae, source_url: url, media_items: media_items
+    { title: title, body: body, source: :tn, source_url: url, media_items: media_items }
   end
 
 end
