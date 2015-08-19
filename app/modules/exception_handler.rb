bdbdd408e60a46cc5769895028c2d86aed5510ca
Aldana Quintana <aldanaqm@gmail.com>
2015-08-18 22:42:28 -0300

    Translations of error messages + consistent error handling

--- app/modules/exception_handler.rb
+++ app/modules/exception_handler.rb
@@ -3,10 +3,16 @@ module ExceptionHandler
 
   included do
     rescue_from BusinessRuleError, with: :busines_rule_error_handler
+    rescue_from CanCan::AccessDenied, with: :cancan_error_handler
   end
 
   def busines_rule_error_handler(exception)
     flash[:danger] = I18n.t(exception.key)
+    redirect_to root_path
+  end
+
+  def cancan_error_handler(exception)
+    flash[:danger] = I18n.t(".error_not_authorized")
     redirect_to root_path
   end
 
