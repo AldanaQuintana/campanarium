32d8ef2c7c8c0e36121fc74435ee66d95b89c661
Aldana Quintana <aldanaqm@gmail.com>
2015-08-22 18:00:04 -0300

    Basic views (only files) and action on controller (provisional)

--- app/controllers/notice_controller.rb
+++ app/controllers/notice_controller.rb
@@ -1,5 +1,7 @@
 class NoticeController < ApplicationController
   def index
     authorize! :noticias, :index
+    # Esto debería cambiar en un futuro, no hay que traer todos los grupos
+    @notice_groups = NoticeGroup.all
   end
 end
