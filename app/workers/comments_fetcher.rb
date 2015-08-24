ab2fddcd78b28baf3a2ec852b06390459ba8e375
pablo31 <psfutn@gmail.com>
2015-08-23 22:48:50 -0300

    comments fetcher searchs 100 tweets from a keyword

--- app/workers/comments_fetcher.rb
+++ app/workers/comments_fetcher.rb
@@ -9,4 +9,15 @@ class CommentsFetcher < ResqueJob
     end
   end
 
+  def search_and_persist str
+    tweets = client.search(str).attrs[:statuses]
+    tweets.each do |tweet|
+      message = tweet[:text]
+      username = tweet[:user][:name]
+      uuid = tweet[:id_str]
+      puts "Twitter comment found (#{uuid}): #{username} says '#{message}'"
+      Comment.create uuid: uuid, message: message, username: username, source: :twitter
+    end
+  end
+
 end
