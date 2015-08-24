f6634b6fa2ceab7aac1b6d7fbddcd63268053d01
pablo31 <psfutn@gmail.com>
2015-08-23 22:38:08 -0300

    twitter gem, comment model & comments fetcher basic struct

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822173221) do
+ActiveRecord::Schema.define(version: 20150824005718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "source"
+    t.text   "message"
+    t.string "username"
+    t.string "uuid"
+  end
 
   create_table "media", force: :cascade do |t|
     t.string   "file"
