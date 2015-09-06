6b5eae21ed0fba4ec8ef5f1ad4ab252ebb08e2e4
Aldana Quintana <aldanaqm@gmail.com>
2015-09-06 20:18:10 -0300

    Unlink and destroy notice taking into account its notice group

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,5 +1,6 @@
 class Notice < ActiveRecord::Base
   has_many :media, as: :media_owner, class_name: Media
+  before_destroy :destroy_group_if_empty
   belongs_to :notice_group
 
   def images_urls
@@ -8,5 +9,15 @@ class Notice < ActiveRecord::Base
 
   def has_images?
     images_urls.reject(&:nil?).length > 0
+  end
+
+  def unlink!
+    self.destroy_group_if_empty
+    self.notice_group_id = nil
+    self.save
+  end
+
+  def destroy_group_if_empty
+    notice_group.destroy unless notice_group.notices.where("id != ?", self.id).count > 0
   end
 end
