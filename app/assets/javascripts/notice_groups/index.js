027bf9533690388ffd3d0fef9dc84cea20d448ad
Aldana Quintana <aldanaqm@gmail.com>
2015-09-09 20:54:32 -0300

    Prepend partials correctly and show spinner

--- app/assets/javascripts/notice_groups/index.js
+++ app/assets/javascripts/notice_groups/index.js
@@ -3,21 +3,21 @@
   var page = 1;
 
   $("#end-of-notices").bind("inview", function(event, isInView, visiblePartX, visiblePartY){
-    if(isInView){
-      page += 1;
-      $.ajax({
-        url: "/noticias",
-        method: "GET",
-        data: {
-          page: page
-        }
-      }).then(function(data){
-        var html_partial = _.reduce(data.groups, function(result, group){
-          result += group.html_partial[0];
-          return result;
-        }, "");
-        $("#end-of-notices")[0].innerHTML += html_partial;
-      });
-    }
+    $(".fa.fa-spinner").removeClass("hidden");
+    page += 1;
+    $.ajax({
+      url: "/noticias",
+      method: "GET",
+      data: {
+        page: page
+      }
+    }).then(function(data){
+      $(".fa.fa-spinner").addClass("hidden");
+      var html_partial = _.reduce(data.groups, function(result, group){
+        result += group.html_partial[0];
+        return result;
+      }, "");
+      $("#notice_index").append(html_partial);
+    });
   })
 })();
