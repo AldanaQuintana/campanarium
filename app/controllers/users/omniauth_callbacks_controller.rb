5c49f3914005b20990db525fb55feac8efc68cb5
Daniela Kazarian <dkazarian@redmintlabs.com>
2015-09-10 18:24:53 -0300

    Avatar from social media

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,10 +14,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     fake_email = Faker::Internet.email(Faker::Internet.user_name + DateTime.now.strftime("%Y%m%d%H%M%S"))
     fake_pwd = Devise.friendly_token[0,20]
     if provider == :facebook
-      {name: data.info.name, email: data.info.email, password: fake_pwd, has_password: false}
+      user_params = {name: data.info.name, email: data.info.email, password: fake_pwd, has_password: false}
+      image_url = data.info.image.gsub("http:","https:") + "?type=large"
     else provider == :twitter
-      {name: data.info.name, email: fake_email , password: fake_pwd, has_password: false, has_email: false}
+      user_params = {name: data.info.name, email: fake_email , password: fake_pwd, has_password: false, has_email: false}
+      image_url = data.info.image.gsub("http:","https:").gsub("normal","400x400")
     end
+    return user_params, image_url
   end
 
   def handle_omniauth provider
@@ -32,14 +35,23 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => provider) if is_navigational_format?
     else #Registration
-      @user = User.new(extract_data(provider, oauth_data))
+      user_params, image_url = extract_data(provider, oauth_data)
+      @user = User.new(user_params)
       @user.save!
+      save_avatar @user, image_url
       @user.link_oauth oauth_data
       sign_in @user
       redirect_to notice_groups_path
     end
   end
 
+  def save_avatar user, image_url
+    media = Media.new
+    media.remote_image_url = image_url
+    media.media_owner = user 
+    media.save!
+    
+  end
 
 
   # You should also create an action method in this controller like this:
