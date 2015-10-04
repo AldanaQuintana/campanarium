1e87ea0af884471546d8776be604f143095f1200
Aldana Quintana <aldanaqm@gmail.com>
2015-10-04 18:26:17 -0300

    Manage some errors

--- app/modules/semantic_analyzer_connector.rb
+++ app/modules/semantic_analyzer_connector.rb
@@ -5,17 +5,21 @@ class SemanticAnalyzerConnector
         corpus: [],
         metadata: {}
       }
-      notices = Notice.where(notice_group_id: nil)
+      notices = Notice.where(notice_group_id: nil).limit(50)
       notices.each do |notice|
-        body[:corpus].push({
-          document: notice.body,
-          category: notice.categories[0],
-          keywords: notice.keywords,
-          user_info: notice.id
-        })
+        if(notice.body.present?)
+          body[:corpus].push({
+            document: notice.body,
+            category: notice.categories[0],
+            keywords: notice.keywords,
+            user_info: notice.id
+          })
+        end
       end
 
-      body[:metadata][:nmb_of_centroids] = (notices.length / notices.pluck(:source).uniq.count).to_i
+      if(body[:corpus].length == 0)then puts("No notices to group"); return; end
+
+      body[:metadata][:nmb_of_centroids] = [(notices.length / notices.pluck(:source).uniq.count).to_i, 5].min
       from = Time.now
       code, response = ExternalServiceCall.new().post(url("/analyzer"), body)
       duration = Time.now - from
@@ -24,6 +28,8 @@ class SemanticAnalyzerConnector
         response["result_set"]["result"].each do |result|
           NoticeGroup.create(notices: Notice.find(result["grouped_documents"].map{|document| document["user_info"]}))
         end
+      else
+        puts("Request failed: #{response['error']}" )
       end
     end
 
