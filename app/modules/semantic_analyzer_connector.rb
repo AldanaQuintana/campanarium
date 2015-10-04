496bb542dc718e47d732606505c595d595939e0f
Aldana Quintana <aldanaqm@gmail.com>
2015-10-04 17:36:39 -0300

    Persist clusters of notices

--- app/modules/semantic_analyzer_connector.rb
+++ app/modules/semantic_analyzer_connector.rb
@@ -1,30 +1,30 @@
 class SemanticAnalyzerConnector
   class << self
     def group_notices
-      # body = {
-      #   corpus: [],
-      #   metadata: {
-      #   nmb_of_centroids: 2
-      #   }
-      # }
-
-      notice = Notice.first
       body = {
-        corpus: [
-          {
-            document: notice.body,
-            category: notice.categories[0],
-            keywords: notice.keywords,
-            user_info: notice.id
-          },
-          { document: "foo2", category: "politics", keywords: ["foo"], user_info: "user_info_1" }
-        ],
-        metadata: {
-          nmb_of_centroids: 2
-        }
+        corpus: [],
+        metadata: {}
       }
+      notices = Notice.where(notice_group_id: nil)
+      notices.each do |notice|
+        body[:corpus].push({
+          document: notice.body,
+          category: notice.categories[0],
+          keywords: notice.keywords,
+          user_info: notice.id
+        })
+      end
 
-      ExternalServiceCall.new().post(url("/analyzer"), body)
+      body[:metadata][:nmb_of_centroids] = (notices.length / notices.pluck(:source).uniq.count).to_i
+      from = Time.now
+      code, response = ExternalServiceCall.new().post(url("/analyzer"), body)
+      duration = Time.now - from
+      puts("Duración: #{duration}")
+      if(code == 200){
+        response["result_set"]["result"].each do |result|
+          NoticeGroup.create(notices: Notice.find(result["grouped_documents"].map{|document| document["user_info"]}))
+        end
+      }
     end
 
     def url(route)
