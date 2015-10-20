21738b613130042252b75927684efffdba6c5454
pablo31 <psfutn@gmail.com>
2015-10-19 22:54:54 -0300

    no categories for minuto uno..

--- app/workers/minuto_uno_fetcher.rb
+++ app/workers/minuto_uno_fetcher.rb
@@ -36,14 +36,14 @@ class MinutoUnoFetcher < SourceFetcher
     html = Nokogiri::HTML open url
     title = html.css('.article-detail-heading .title').text
     body = p_body_from html.css '.article-content'
-    categories = html.css('section.article-detail-heading .tag.one-line').text
+    # categories = html.css 'section.article-detail-heading .tag.one-line'
     keywords = html.css('section.extra-tag-list .tag-list .link').map &:text
     image = html.css('.gallery-area img').first
     image = image && image.attr('src')
     writed_at_str = html.css('.article-detail-heading .date').text.gsub 'de', '' # Formato: '31 de julio 2015 - 18:29'
     writed_at = writed_at_str && Time.parse(writed_at_str)
     puts "Updated time text: '#{writed_at_str}' Updated time parsed: #{writed_at}"
-    create_notice title: title, categories: categories, keywords: keywords,
+    create_notice title: title, keywords: keywords,
       url: url, writed_at: writed_at, body: body, media: image
   end
 
