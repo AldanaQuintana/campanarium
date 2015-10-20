f4823a931249a6462af0920c1d4b66b09e4fd58b
Aldana Quintana <aldanaqm@gmail.com>
2015-10-20 20:31:36 -0300

    json mime type

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  JSON_MIME_TYPE = "application/json"
+
   mount Resque::Server, at: '/resque'
 
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations'}
