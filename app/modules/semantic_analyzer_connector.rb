9792af978bd7d00d788cf068e58d308b517e1929
Aldana Quintana <aldanaqm@gmail.com>
2015-10-21 18:46:45 -0300

    Fix connector

--- app/modules/semantic_analyzer_connector.rb
+++ app/modules/semantic_analyzer_connector.rb
@@ -6,21 +6,22 @@ class SemanticAnalyzerConnector
         metadata: {},
         url: AppConfiguration.semantic_analyzer_response_url
       }
-      notices = Notice.where(notice_group_id: nil).limit(20)
+      notices = Notice.where("length(categories) > 0").where(notice_group_id: nil).limit(20)
+
       notices.each do |notice|
         if(notice.body.present?)
           body[:corpus].push({
             document: notice.body.truncate(600, separator: " ").gsub(/(\n|\"|\.\.\.)/, ""),
-            category: notice.categories[0],
+            category: notice.categories[0].name.to_s,
             keywords: notice.keywords,
             user_info: notice.id
           })
         end
       end
-
       if(body[:corpus].length == 0)then puts("No notices to group"); return; end
 
       body[:metadata][:nmb_of_centroids] = [(notices.length / notices.pluck(:source).uniq.count).to_i, 10].min
+
       code, response = ExternalServiceCall.new().post(url("analyzer"), body)
       if(code == 200)
         puts("Request success")
