b82e0e96682529c6fc544a01af034c9f52475ec5
Aldana Quintana <aldanaqm@gmail.com>
2015-10-22 12:43:07 -0300

    Enable load comments for a specific notice group with a specific keyword (optional)

--- app/workers/notice_group_comments_loader.rb
+++ app/workers/notice_group_comments_loader.rb
@@ -2,10 +2,14 @@ class NoticeGroupCommentsLoader
 
   def self.perform
     NoticeGroup.without_comments.limit(10).each do |notice_group|
-      keyword = notice_group.best_keyword
-      if keyword.present?
-        CommentsFetcher.new.search_and_persist keyword, notice_group.id
-      end
+      NoticeGroupCommentsLoader.load_comments_for(notice_group)
+    end
+  end
+
+  def self.load_comments_for(notice_group, keyword = nil)
+    keyword ||= notice_group.best_keyword
+    if keyword.present?
+      CommentsFetcher.new.search_and_persist keyword, notice_group.id
     end
   end
 end
