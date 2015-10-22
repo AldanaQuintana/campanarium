64bfc392e4b00d84402f2cf505cf17ea4442f61c
Aldana Quintana <aldanaqm@gmail.com>
2015-10-22 13:08:59 -0300

    Add colors to pie chart

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,5 +1,5 @@
 module ChartsHelper
-  def with_labels(results, mapping)
-    results.reduce({}){|memo, (key, val)| memo[mapping[key.to_s]] = val; memo; }
+  def pie_chart_with_labels(results, mapping, colors = nil, id = nil)
+    pie_chart results.reduce({}){|memo, (key, val)| memo[mapping[key.to_s]] = val; memo; }, colors:  colors.present? ? results.keys.map{|k| colors[k]} : [], id: id
   end
 end
