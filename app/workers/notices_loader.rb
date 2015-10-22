57b58a0cad60e2ce1d08a0b6e05f26e60d8bde0c
Aldana Quintana <aldanaqm@gmail.com>
2015-10-22 11:52:04 -0300

    Fetch notices of day

--- app/workers/notices_loader.rb
+++ app/workers/notices_loader.rb
@@ -1,8 +1,8 @@
 class NoticesLoader
   def self.perform(from = nil, to = nil)
     unless from.present? && to.present?
-      to = Time.zone.now
-      from = to - 1.hour
+      to = Time.zone.now.at_beginning_of_day
+      from = to - 1.day
     end
     fetchers = [CronicaFetcher, InfobaeFetcher, LaNacionFetcher, TnFetcher]
     fetchers.each do |fetcher|
